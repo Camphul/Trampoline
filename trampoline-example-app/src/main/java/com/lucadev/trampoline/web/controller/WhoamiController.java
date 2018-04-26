7da5b02b24ef1c09303df14e465cf030ba7cca3a
Luca Camphuisen <luca.camphuisen@hva.nl>
2018-04-26 18:18:58 +0200

    Whoami

---
+++ trampoline-example-app/src/main/java/com/lucadev/trampoline/web/controller/WhoamiController.java
@@ -0,0 +1,25 @@
+package com.lucadev.trampoline.web.controller;
+
+import com.lucadev.trampoline.security.model.User;
+import com.lucadev.trampoline.security.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author <a href="mailto:Luca.Camphuisen@hva.nl">Luca Camphuisen</a>
+ * @since 26-4-18
+ */
+@RestController
+@AllArgsConstructor
+public class WhoamiController {
+
+    private UserService userService;
+
+
+    @GetMapping("/whoami")
+    public User whoami() {
+        return userService.currentUser().get();
+    }
+
+}
