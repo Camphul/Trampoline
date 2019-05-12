2425ecec379cb3173e962b340dd765b6dbcca171
Luca Camphuisen <luca.camphuisen@hva.nl>
2019-05-12 17:57:22 +0200

    JavaDoc

--- example-app/src/main/java/com/lucadev/example/trampoline/web/controller/BlogPostCommentController.java
+++ example-app/src/main/java/com/lucadev/example/trampoline/web/controller/BlogPostCommentController.java
@@ -22,6 +22,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 /**
+ * Controller containing endpoints to handle comments on a blogpost.
+ *
  * @author <a href="mailto:luca@camphuisen.com">Luca Camphuisen</a>
  * @since 5/10/19
  */
@@ -34,11 +36,23 @@ public class BlogPostCommentController {
 	private final BlogPostService blogPostService;
 	private final UserService userService;
 
+	/**
+	 * Find all comments for a given blogposts in pageable form.
+	 * @param blogPost the blogpost to look for comments.
+	 * @param pageable the pagination details provided by the client.
+	 * @return a page containing the comments.
+	 */
 	@GetMapping
 	public Page<BlogPostCommentDto> findAllByBlogPost(@FindById BlogPost blogPost, Pageable pageable) {
 		return MappedPage.of(blogPostService.findAllComments(blogPost, pageable), mapper::toDto);
 	}
 
+	/**
+	 * Submit a comment to a blogpost.
+	 * @param blogPost the blogpost to add the comment to.
+	 * @param request the DTO containing the comment details.
+	 * @return the UUID in the form of a {@link UUIDDto} of the created comment.
+	 */
 	@PostMapping
 	@LogUserActivity
 	@PrePolicy("BLOGPOST_COMMENT_SUBMIT")
@@ -49,11 +63,23 @@ public class BlogPostCommentController {
 		return new UUIDDto(comment.getId());
 	}
 
+	/**
+	 * Get details of a specific blogpost comment.
+	 * @param blogPost the blogpost.
+	 * @param comment the comment inside the blogpost.
+	 * @return the comment.
+	 */
 	@GetMapping("/{comment}")
 	public BlogPostCommentDto findById(@FindById BlogPost blogPost, @FindById BlogPostComment comment) {
 		return mapper.toDto(comment);
 	}
 
+	/**
+	 * Delete a certain comment.
+	 * @param blogPost the blogpost in which the comment exists.
+	 * @param comment the comment to delete.
+	 * @return an OK response when deleted.
+	 */
 	@DeleteMapping("/{comment}")
 	@PrePolicy("BLOGPOST_COMMENT_DELETE")
 	@LogUserActivity(value = "'Deleting comment ' + comment.id", spel = true)
