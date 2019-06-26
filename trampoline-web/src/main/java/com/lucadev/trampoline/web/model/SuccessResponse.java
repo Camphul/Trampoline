629820cb4c4f38a75fbc80a3611e3f52f13aa458
Luca Camphuisen <luca.camphuisen@hva.nl>
2019-06-26 08:10:42 +0200

    Code improvements

--- trampoline-web/src/main/java/com/lucadev/trampoline/web/model/SuccessResponse.java
+++ trampoline-web/src/main/java/com/lucadev/trampoline/web/model/SuccessResponse.java
@@ -1,9 +1,5 @@
 package com.lucadev.trampoline.web.model;
 
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.ToString;
-
 /**
  * Extension of {@link MessageResponse} to include a status flag to notify of success or
  * failure.
@@ -11,9 +7,6 @@ import lombok.ToString;
  * @author <a href="mailto:luca@camphuisen.com">Luca Camphuisen</a>
  * @since 21-4-18
  */
-@Getter
-@ToString
-@EqualsAndHashCode
 public class SuccessResponse extends MessageResponse {
 
 	/**
