43c3ca3f6e39d4f77e01d7338b5d45a6c9a74c86
Luca Camphuisen <luca.camphuisen@hva.nl>
2019-06-28 09:43:36 +0200

    Configure bean overrides in environment post processor instead of manually in properties.

--- trampoline-core-autoconfigure/src/main/java/com/lucadev/trampoline/autoconfigure/TrampolineEnvironmentPostProcessor.java
+++ trampoline-core-autoconfigure/src/main/java/com/lucadev/trampoline/autoconfigure/TrampolineEnvironmentPostProcessor.java
@@ -6,7 +6,8 @@ import org.springframework.boot.env.EnvironmentPostProcessor;
 import org.springframework.core.env.ConfigurableEnvironment;
 
 /**
- * Autoconfigure bean overrides to true so we do not have to set it in application properties.
+ * Autoconfigure bean overrides to true so we do not have to set it in application
+ * properties.
  *
  * @author <a href="mailto:luca@camphuisen.com">Luca Camphuisen</a>
  * @since 6/28/19
@@ -20,8 +21,10 @@ public class TrampolineEnvironmentPostProcessor implements EnvironmentPostProces
 	 * @param application our app.
 	 */
 	@Override
-	public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
+	public void postProcessEnvironment(ConfigurableEnvironment environment,
+			SpringApplication application) {
 		log.debug("Configuring trampoline environment.");
 		application.setAllowBeanDefinitionOverriding(true);
 	}
+
 }
