4e51d2d0d6b0fda3753fb909b425727955698ba0
Luca Camphuisen <luca.camphuisen@hva.nl>
2019-08-02 21:08:54 +0200

    Method registry to increase performance of GDPR compliance method intercepts.

--- trampoline-data/src/main/java/com/lucadev/trampoline/data/gdpr/repository/CompliantRepositoryMethodInterceptor.java
+++ trampoline-data/src/main/java/com/lucadev/trampoline/data/gdpr/repository/CompliantRepositoryMethodInterceptor.java
@@ -7,7 +7,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
 
+import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Method interceptor which encrypts if necessary.
@@ -21,25 +26,75 @@ public class CompliantRepositoryMethodInterceptor implements MethodInterceptor {
 
 	private final StringCrypto stringCrypto;
 
+	// Cache to fasten up invocation.
+	private final Map<String, int[]> METHOD_INTERCEPTS = new HashMap<>();
+
 	@Override
 	public Object invoke(MethodInvocation invocation) throws Throwable {
-		Parameter[] parameters = invocation.getMethod().getParameters();
+		String methodDescription = getMethodDescription(invocation.getMethod());
+		if (!this.METHOD_INTERCEPTS.containsKey(methodDescription)) {
+			return invocation.proceed();
+		}
+
 		Object[] arguments = invocation.getArguments();
+		for (int paramIndex : this.METHOD_INTERCEPTS.get(methodDescription)) {
+			arguments[paramIndex] = this.stringCrypto
+					.encrypt((String) arguments[paramIndex]);
+		}
+
+		// Proceed
+		return invocation.proceed();
+	}
+
+	/**
+	 * Registers methods which we will intercept. Will improve performance.
+	 *
+	 * @param method method to cache.
+	 */
+	public void registerQueryMethodForIntercept(Method method) {
+		int[] indices = resolvePersonalDataParameterIndices(method.getParameters());
+		if (indices.length == 0) {
+			throw new IllegalStateException(
+					"Cannot register method which does not include @PersonalData");
+		}
+		String methodDescription = getMethodDescription(method);
+		this.METHOD_INTERCEPTS.put(methodDescription, indices);
+		log.debug("Registered query method {} in cache.", methodDescription);
+	}
+
+	/**
+	 * Resolve parameter indices which need encryption.
+	 *
+	 * @param parameters the method parameters.
+	 * @return array of indices which require encryption.
+	 */
+	private int[] resolvePersonalDataParameterIndices(Parameter[] parameters) {
+		List<Integer> indices = new ArrayList<>(parameters.length);
+
+		// Loop through params
 		for (int i = 0; i < parameters.length; i++) {
 			Parameter parameter = parameters[i];
-			// Check if we need to encrypt before execution
+			// Check if we need to encrypt
 			if (parameter.isAnnotationPresent(PersonalData.class)) {
 				if (!parameter.getType().equals(String.class)) {
 					throw new IllegalArgumentException("Argument \"" + parameter.getName()
 							+ "\" with @PersonalData must be of type String.");
 				}
-				// Encrypt
-				arguments[i] = this.stringCrypto.encrypt((String) arguments[i]);
-				log.debug("Encrypted repository method argument in intercept.");
+				indices.add(i);
 			}
 		}
-		// Proceed
-		return invocation.proceed();
+		return indices.stream().mapToInt(Integer::intValue).toArray();
+	}
+
+	/**
+	 * Not to be confused with Java method descriptions. This simply contains classname
+	 * and methodname.
+	 *
+	 * @param method method to get description of.
+	 * @return classname and method name.
+	 */
+	private String getMethodDescription(Method method) {
+		return method.getDeclaringClass().getName() + "#" + method.getName();
 	}
 
 }
--- trampoline-data/src/main/java/com/lucadev/trampoline/data/gdpr/repository/CompliantRepositoryProxyPostProcessor.java
+++ trampoline-data/src/main/java/com/lucadev/trampoline/data/gdpr/repository/CompliantRepositoryProxyPostProcessor.java
@@ -1,10 +1,15 @@
 package com.lucadev.trampoline.data.gdpr.repository;
 
+import com.lucadev.trampoline.data.gdpr.PersonalData;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.aop.framework.ProxyFactory;
 import org.springframework.data.repository.core.RepositoryInformation;
 import org.springframework.data.repository.core.support.RepositoryProxyPostProcessor;
+import org.springframework.data.util.Streamable;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 /**
  * Registers our advice to repository interfaces.
@@ -22,8 +27,21 @@ public class CompliantRepositoryProxyPostProcessor
 	@Override
 	public void postProcess(ProxyFactory factory,
 			RepositoryInformation repositoryInformation) {
+		// Check if we need to register a method interceptor on given repository.
+		Streamable<Method> methods = repositoryInformation.getQueryMethods()
+				.filter(this::hasAnyPersonalData);
+		if (methods.isEmpty()) {
+			return;
+		}
 		log.info("Adding GDPR compliant repository advice.");
+		methods.forEach(
+				this.compliantRepositoryMethodInterceptor::registerQueryMethodForIntercept);
 		factory.addAdvice(this.compliantRepositoryMethodInterceptor);
+	}
+
+	private boolean hasAnyPersonalData(Method method) {
+		return Arrays.stream(method.getParameters())
+				.anyMatch(parameter -> parameter.isAnnotationPresent(PersonalData.class));
 	}
 
 }
